4684ac8b43d19cec98b6de762aa7448074f37729
ZohirB <zohirboshi@gmail.com>
2023-08-05 12:15:38 +0300

    main

--- WindowsExplorer/WindowsExplorer/Program.cs
+++ WindowsExplorer/WindowsExplorer/Program.cs
@@ -1,25 +1,20 @@
+using API.FileProcessing.Service;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddTransient<IManageImage, ManageImage>();
 
-// Configure the HTTP request pipeline.
+
+var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
